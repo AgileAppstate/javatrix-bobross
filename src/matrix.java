b46dec68f3fd13ca30aac9e5f2c8579f45a8c9a9
bruchpilot123 <s191818@student.dhbw-mannheim.de>
2021-09-28 11:53:33 -0400

    Added the print

--- src/matrix.java
+++ src/matrix.java
@@ -4,6 +4,14 @@ public class Matrix {
     private Matrix(double[][] A){
         matrix = A;
     }
-
-
+    
+    public void print(int a, int b){
+		for(int i=0; i<a;i++){
+			for(int j=0; j<b; j++){
+				System.out.print(matrix[i][j]+ " ");
+					}
+			System.out.println();			
+			}
+	}
+	
 }
