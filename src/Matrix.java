0975046e454f8136ed6b3d41d45e0e88831c6c2a
Andrew Pobrica <pirit66@gmail.com>
2021-09-28 16:54:07 -0400

    Finished work on print, needs verification

--- src/Matrix.java
+++ src/Matrix.java
@@ -6,9 +6,10 @@ public class Matrix {
     }
     
     public void print(int a, int b){
-		for(int i=0; i<a;i++){
-			for(int j=0; j<b; j++){
-				System.out.print(matrix[i][j]+ " ");
+        String format = "%" + a + "." + b + "f"; 
+		for(int i=0; i < matrix.length;i++){
+			for(int j=0; j < matrix[0].length; j++){
+				System.out.printf(format, matrix[i][j]);
 					}
 			System.out.println();			
 			}
